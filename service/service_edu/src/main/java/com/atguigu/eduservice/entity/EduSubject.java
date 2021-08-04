1d339ffddc3f23e5658f664f5da88a58191c9ad0
sgd <smile_boy@foxmail.com>
2021-08-04 17:33:33 +0800

    2

---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/entity/EduSubject.java
@@ -0,0 +1,44 @@
+package com.atguigu.eduservice.entity;
+
+import java.util.Date;
+import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * <p>
+ * 课程科目
+ * </p>
+ *
+ * @author atguigu
+ * @since 2021-08-04
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ApiModel(value="EduSubject对象", description="课程科目")
+public class EduSubject implements Serializable {
+
+    private static final long serialVersionUID=1L;
+
+    @ApiModelProperty(value = "课程类别ID")
+    private String id;
+
+    @ApiModelProperty(value = "类别名称")
+    private String title;
+
+    @ApiModelProperty(value = "父ID")
+    private String parentId;
+
+    @ApiModelProperty(value = "排序字段")
+    private Integer sort;
+
+    @ApiModelProperty(value = "创建时间")
+    private Date gmtCreate;
+
+    @ApiModelProperty(value = "更新时间")
+    private Date gmtModified;
+
+
+}
